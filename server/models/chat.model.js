ffba4bc408acbf158669a2955e08f7c09cacd40b
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-02-22 20:49:54 +0530

    day-2

---
+++ server/models/chat.model.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const ono_chatSchema = mongoose.Schema({
+    "userid":{"type": String, "required": true},
+    "msg":{"type": String, "required": true},
+    ""
+});
