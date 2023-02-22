3b44566cc96e80eded663e99893636931ac6192e
Dilip Sanap <112867608+dilipsanapinb@users.noreply.github.com>
2023-02-22 16:33:54 +0530

    basic routes added

--- Public/Scripts/client.js
+++ Public/Scripts/client.js
@@ -4,13 +4,7 @@ let messageArea=document.querySelector(".message_area");
 
 
 //giving name to user*****
-// let nam=document.getElementById("");
-// let nam
-// do{
-//     nam=prompt("Please enter your nam :")
-// }while(!nam);
-
-
+let nam=document.getElementById("");
 
 textArea.addEventListener('keyup',(e)=>{
     if(e.key==="Enter"){
@@ -34,18 +28,19 @@ scrollToBottom();
     socket.emit('message', msg)
 }
 
-// function appendMessage(msg,type){
-//     let mainDiv=document.createElement('div')
-//     let className=type;
-//     mainDiv.classList.add(className,'message');
+function appendMessage(msg,type){
+    let mainDiv=document.createElement('div')
+    let className=type;
+    mainDiv.classList.add(className,'message');
 
-//     let markup=`
-//     <h4>${msg.user}</h4>
-//     <p>${msg.message}</p>
-//     `
-//     mainDiv.innerHTML=markup;
-//     messageArea.appendChild(mainDiv)
-// }
+    // appending msg to each div
+    let markup=`
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML=markup;
+    messageArea.appendChild(mainDiv)
+}
 
 // *****receive Message****
 socket.on("message",(msg)=>{
