080bcb23d749063f9dfc97fa4605fc84299268bc
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2023-02-26 19:21:19 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -1,87 +1,87 @@
-const express = require("express");
-const app = express();
-const { connection } = require("./config/db");
-const http = require("http");
-const colors = require("colors");
-require("dotenv").config();
-const { userRouter } = require("./routes/user.route");
-const { chatRouter } = require("./routes/chat.route");
-const path = require("path");
-const { messageRouter } = require("./routes/messageroute");
-const server = http.createServer(app);
-const PORT = process.env.port;
+// const express = require("express");
+// const app = express();
+// const { connection } = require("./config/db");
+// const http = require("http");
+// const colors = require("colors");
+// require("dotenv").config();
+// const { userRouter } = require("./routes/user.route");
+// const { chatRouter } = require("./routes/chat.route");
+// const path = require("path");
+// const { messageRouter } = require("./routes/messageroute");
+// const server = http.createServer(app);
+// const PORT = process.env.port;
 
 
 
-app.use("/user", userRouter);
-app.use("/chat", chatRouter);
-app.use("/message", messageRouter);
+// app.use("/user", userRouter);
+// app.use("/chat", chatRouter);
+// app.use("/message", messageRouter);
 
-server.listen(PORT, async () => {
-  try {
-    await connection;
-    console.log("Connected To Db".cyan.underline);
-  } catch (error) {
-    console.log("Not Connected To Db".red.underline);
-    console.log(error);
-  }
-  console.log(`Server is listenig on :${PORT}`.yellow.bold);
-});
+// server.listen(PORT, async () => {
+//   try {
+//     await connection;
+//     console.log("Connected To Db".cyan.underline);
+//   } catch (error) {
+//     console.log("Not Connected To Db".red.underline);
+//     console.log(error);
+//   }
+//   console.log(`Server is listenig on :${PORT}`.yellow.bold);
+// });
 
-// Boat Name
-const boatName = "Chit-Chat App";
-
-
-// Directory Part
-const STATIC_FILES_PATH = path.resolve("../frontend");
-app.use(express.static(STATIC_FILES_PATH));
-
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("Connected to socket.io");
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
-
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("User Joined Room: " + room);
-  });
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-  socket.on("new message", (newMessageRecieved) => {
-    let chat = newMessageRecieved.chat;
-
-    if (!chat.users) return console.log("chat.users not defined");
-
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
-
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
-  });
-
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-  });
-
-  socket.off("setup",()=>{
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-    });
-
-});
+// // Boat Name
+// const boatName = "Chit-Chat App";
 
 
-// ****************Expreriment
+// // Directory Part
+// const STATIC_FILES_PATH = path.resolve("../frontend");
+// app.use(express.static(STATIC_FILES_PATH));
+
+// const io = require("socket.io")(server, {
+//   pingTimeout: 60000,
+//   cors: {
+//     origin: "http://localhost:3000",
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("Connected to socket.io");
+//   socket.on("setup", (userData) => {
+//     socket.join(userData._id);
+//     socket.emit("connected");
+//   });
+
+//   socket.on("join chat", (room) => {
+//     socket.join(room);
+//     console.log("User Joined Room: " + room);
+//   });
+//   socket.on("typing", (room) => socket.in(room).emit("typing"));
+//   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+//   socket.on("new message", (newMessageRecieved) => {
+//     let chat = newMessageRecieved.chat;
+
+//     if (!chat.users) return console.log("chat.users not defined");
+
+//     chat.users.forEach((user) => {
+//       if (user._id == newMessageRecieved.sender._id) return;
+
+//       socket.in(user._id).emit("message recieved", newMessageRecieved);
+//     });
+//   });
+
+//   socket.off("setup", () => {
+//     console.log("USER DISCONNECTED");
+//     socket.leave(userData._id);
+//   });
+
+//   socket.off("setup",()=>{
+//     console.log("USER DISCONNECTED");
+//     socket.leave(userData._id);
+//     });
+
+// });
+
+
+// // ****************Expreriment
 
  
--- frontend/scripts/cliient.js
+++ frontend/scripts/cliient.js
@@ -1,201 +1,201 @@
-// Searching for User in serach box[left side menu]
+// // Searching for User in serach box[left side menu]
 
-// const { Socket } = require("socket.io");
-const socket = io("http://localhost:8080/", { transports: ["websocket"] });
-let url = "http://localhost:8080";
-let name = "Dilip";
-let getData = async () => {
-  const data = await fetch(`http://localhost:8080/user`); // pavan
-  let a = await data.json();
-  console.log(a);
-};
-getData();
+// // const { Socket } = require("socket.io");
+// const socket = io("http://localhost:8080/", { transports: ["websocket"] });
+// let url = "http://localhost:8080";
+// let name = "Dilip";
+// let getData = async () => {
+//   const data = await fetch(`http://localhost:8080/user`); // pavan
+//   let a = await data.json();
+//   console.log(a);
+// };
+// getData();
 
-//////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 
-// To Access Chat from list of chats[left side container] [selectin the person]
-// it will give the created chat
-try {
-  const config = {
-    headers: {
-      "content-type": "application/json",
-    },
-  };
-  const { data } = await axios.post(`http://localhost:8080/chat`, { userId }, config);
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// // To Access Chat from list of chats[left side container] [selectin the person]
+// // it will give the created chat
+// try {
+//   const config = {
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//   };
+//   const { data } = await axios.post(`http://localhost:8080/chat`, { userId }, config);
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-//////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 
-// fetching all the chats of the user
+// // fetching all the chats of the user
 
-try {
-  const { data } = await axios.get("/chat");
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.get("/chat");
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-//////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 
-// craeting a group with users from db
+// // craeting a group with users from db
 
-try {
-  const { data } = await axios.get(`/users?search=${searchingData}`);
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.get(`/users?search=${searchingData}`);
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-try {
-  const { data } = await axios.post(`/chat/group`, {
-    name: groupChatName,
-    users: JSON.stringify(selectedUsers.map((u) => u._id)),
-  });
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.post(`/chat/group`, {
+//     name: groupChatName,
+//     users: JSON.stringify(selectedUsers.map((u) => u._id)),
+//   });
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////
 
-//  Creating group
+// //  Creating group
 
-// To Rename the Group Name
+// // To Rename the Group Name
 
-try {
-  const { data } = await axios.put(`/chat/renamegroup`, {
-    chatId: selectedChat._id,
-    chatName: groupChatName,
-  });
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.put(`/chat/renamegroup`, {
+//     chatId: selectedChat._id,
+//     chatName: groupChatName,
+//   });
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-//   searching the user for adding to group
+// //   searching the user for adding to group
 
-try {
-  const { data } = await axios.get(`/users?search=${searchingData}`);
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.get(`/users?search=${searchingData}`);
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-//   adding the user to group
+// //   adding the user to group
 
-try {
-  const { data } = await axios.put(`/chat/addToGroup`, {
-    chatId: selectedChat._id,
-    userId: user1._id,
-  });
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.put(`/chat/addToGroup`, {
+//     chatId: selectedChat._id,
+//     userId: user1._id,
+//   });
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-// Remove the user from group
+// // Remove the user from group
 
-try {
-  const { data } = await axios.put(`/chat/removeFromGroup`, {
-    chatId: selectedChat._id,
-    userId: user1._id,
-  });
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.put(`/chat/removeFromGroup`, {
+//     chatId: selectedChat._id,
+//     userId: user1._id,
+//   });
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////
 
-//  sendiing a message
+// //  sendiing a message
 
-try {
-  const config = {
-    headers: {
-      "content-type": "application/json",
-    },
-  };
-  const { data } = await axios.post(
-    `/message`,
-    { content: "newmessage", chatId: selectedChat._id },
-    config
-  );
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const config = {
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//   };
+//   const { data } = await axios.post(
+//     `/message`,
+//     { content: "newmessage", chatId: selectedChat._id },
+//     config
+//   );
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////
 
-// Fetching all messages
+// // Fetching all messages
 
-try {
-  const { data } = await axios.get(`/message/${id}`);
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const { data } = await axios.get(`/message/${id}`);
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
 
-//////////////////////////////////////////
-/*
- for Socket.io
- install npm i socket.io-client
- inside js file
+// //////////////////////////////////////////
+// /*
+//  for Socket.io
+//  install npm i socket.io-client
+//  inside js file
 
-*/
-const io = require("socket.io-client");
-const endpoint = "url of backend server";
-// let socket, selectedChatCompare;
+// */
+// const io = require("socket.io-client");
+// const endpoint = "url of backend server";
+// // let socket, selectedChatCompare;
 
-socket = io(endpoint);
-// it should give coneted to socket.io in a backend
-socket.emit("setup", user);
-socket.on("connected", () => setSocketConnected(true));
-socket.on("typing", () => setIsTyping(true));
-socket.on("stop typing", () => setIsTyping(false));
+// socket = io(endpoint);
+// // it should give coneted to socket.io in a backend
+// socket.emit("setup", user);
+// socket.on("connected", () => setSocketConnected(true));
+// socket.on("typing", () => setIsTyping(true));
+// socket.on("stop typing", () => setIsTyping(false));
 
-// fetching the data
-socket.emit("join chat", selectedChat._id);
+// // fetching the data
+// socket.emit("join chat", selectedChat._id);
 
-socket.on("message recieved", (newMessageRecieved) => {
-  if (
-    !selectedChatCompare ||
-    selectedChatCompare._id !== newMessageRecieved.chat._id
-  ) {
-    //give notification
-  } else {
-    setMessages([...messages, newMessafeRevieved]);
-  }
-});
+// socket.on("message recieved", (newMessageRecieved) => {
+//   if (
+//     !selectedChatCompare ||
+//     selectedChatCompare._id !== newMessageRecieved.chat._id
+//   ) {
+//     //give notification
+//   } else {
+//     setMessages([...messages, newMessafeRevieved]);
+//   }
+// });
 
-//inside send message function
+// //inside send message function
 
-socket.emit("new message", "data from api /message");
+// socket.emit("new message", "data from api /message");
 
-//typing funcationality
+// //typing funcationality
 
-if (!socketConnected) return;
+// if (!socketConnected) return;
 
-if (!typing) {
-  setTyping(true);
-  socket.emit("typing", selectedChat._id);
-}
-let lastTypingTime = new Date().getTime();
-var timerLength = 3000;
-setTimeout(() => {
-  var timeNow = new Date().getTime();
-  var timeDiff = timeNow - lastTypingTime;
-  if (timeDiff >= timerLength && typing) {
-    socket.emit("stop typing", selectedChat._id);
-    setTyping(false);
-  }
-}, timerLength);
+// if (!typing) {
+//   setTyping(true);
+//   socket.emit("typing", selectedChat._id);
+// }
+// let lastTypingTime = new Date().getTime();
+// var timerLength = 3000;
+// setTimeout(() => {
+//   var timeNow = new Date().getTime();
+//   var timeDiff = timeNow - lastTypingTime;
+//   if (timeDiff >= timerLength && typing) {
+//     socket.emit("stop typing", selectedChat._id);
+//     setTyping(false);
+//   }
+// }, timerLength);
 
-//stop te typing icon after typing
-socket.emit("stop typing", selectedChat._id);
+// //stop te typing icon after typing
+// socket.emit("stop typing", selectedChat._id);
 
 
 
