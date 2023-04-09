cc19d9307355351cdcbc914e952033938d02a802
dilipsanapinb <dilipinbsanap@gmail.com>
2023-04-09 21:05:09 +0530

    main file updated

--- Backend/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
-require('dotenv').config()
-const connection = mongoose.connect(process.env.MOngoURl);
-
-module.exports={connection}
